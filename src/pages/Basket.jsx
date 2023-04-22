d00103a900cf3cef8716630abd4df4e074e45f6c
charles.simon-meunier <charles.simon-meunier@epita.fr>
2023-04-22 23:24:12 +0200

    Edit basket items display

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import "../styles/Basket.scss";
 
 export default function Basket() {
   const basket = useSelector((state) => state.basket);
   return (
     <main>
-      <section>
+      <section className="basket">
         <h1>Votre panier</h1>
-        {basket?.items.map((product) => (
-          <div key={product.id}>
-            <img src={product.img} alt={product.name} />
-            <h2>{product.name}</h2>
-            <p>{product.price}€</p>
-          </div>
-        ))}
+        <div className="basket_items">
+          {basket?.items.map((product) => (
+            <div key={product.id}>
+              <div className="img">
+                <img src={product.img} alt={product.name} />
+              </div>
+              <h2>{product.name}</h2>
+              <p>{product.price?.toString().replace(".", ",")}€</p>
+            </div>
+          ))}
+        </div>
       </section>
     </main>
   );
